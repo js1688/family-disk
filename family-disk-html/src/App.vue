140170f98551824c0429e8e66827c4e887db949c
tanjun <woshitanjun@icloud.com>
2022-12-21 17:31:27 +0800

    前端项目包架构提交

---
+++ family-disk-html/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+
+  </header>
+  <body>
+    <van-tabbar v-model="active">
+      <van-tabbar-item badge="3">
+        <span>自定义</span>
+        <template #icon="props">
+          <img :src="props.active ? icon.active : icon.inactive" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </body>
+</template>
+<script>
+import { Tabbar, TabbarItem } from 'vant';
+import { ref } from 'vue';
+  export default {
+    name: 'App',
+    setup() {
+      const active = ref(0);
+      const icon = {
+        active: 'https://fastly.jsdelivr.net/npm/@vant/assets/user-active.png',
+        inactive:
+            'https://fastly.jsdelivr.net/npm/@vant/assets/user-inactive.png',
+      };
+      return {
+        icon,
+        active,
+      }
+    },
+    components: {
+      [Tabbar.name]: Tabbar,
+      [TabbarItem.name]: TabbarItem
+
+    },
+    props: {
+    },
+    data: function() {
+      return {
+        name: "谭峻"
+      };
+    },
+    methods: {
+    }
+  }
+</script>
---
+++ family-disk-html/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'vant/lib/index.css';
+const app = createApp(App).mount('#app');
