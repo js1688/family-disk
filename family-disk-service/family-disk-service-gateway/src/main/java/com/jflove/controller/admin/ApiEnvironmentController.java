8cc54515beed0e2e2d259e51da9fd04101aae14a
js1688 <woshitanjun@icloud.com>
2023-01-13 14:42:05 +0800

    获取服务端内网环境地址接口

--- family-disk-service/family-disk-service-gateway/src/main/java/com/jflove/controller/admin/ApiEnvironmentController.java
+++ family-disk-service/family-disk-service-gateway/src/main/java/com/jflove/controller/admin/ApiEnvironmentController.java
@@ -3,12 +3,15 @@ package com.jflove.controller.admin;
 import com.jflove.vo.ResponseHeadVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author: tanjun
@@ -20,9 +23,16 @@ import javax.servlet.http.HttpServletResponse;
 @Api(tags = "api调用网络环境")
 public class ApiEnvironmentController {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @ApiOperation(value = "获取服务端本地环境的ip和端口")
     @GetMapping("/getServiceLocalPath")
     public ResponseHeadVO<String> getServiceLocalPath(HttpServletRequest request, HttpServletResponse response){
-        return new ResponseHeadVO<>("local="+request.getLocalAddr() + ",Remote="+request.getRemoteAddr());
+        try{
+            String addr = InetAddress.getLocalHost().getHostAddress();
+            return new ResponseHeadVO<>(true,String.format("http://%s:%s/",addr,port));
+        }catch (UnknownHostException e){}
+        return new ResponseHeadVO<>(false,"没有获取到内网地址");
     }
 }
