b40f073f1f219ea8f8141804a3102deb7827de28
js1688 <woshitanjun@icloud.com>
2023-02-08 17:50:33 +0800

    表模型
    空间关系审批权限

--- mysql-table-model/DDL/user_space.sql
+++ mysql-table-model/DDL/user_space.sql
@@ -6,7 +6,7 @@ create table user_space
     max_size       int         null comment '最大空间大小(MB)',
     code           varchar(64) null comment '空间编码',
     use_size       int         null comment '已使用空间大小(MB)',
-    title          varchar(64) null comment '空间主题',
+    title          varchar(64) not null comment '空间主题',
     create_user_id int         null comment '空间创建用户id',
     create_time    timestamp   null comment '创建时间',
     update_time    timestamp   null comment '修改时间',
