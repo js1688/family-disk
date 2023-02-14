cd7fc87f667abecb051ee9fb562e859189cca9e9
js1688 <woshitanjun@icloud.com>
2023-02-14 17:56:51 +0800

    解决边下边播,兼容safari浏览器

--- family-disk-service/family-disk-service-gateway/src/main/java/com/jflove/config/ByteResourceHttpRequestHandlerConfig.java
+++ family-disk-service/family-disk-service-gateway/src/main/java/com/jflove/config/ByteResourceHttpRequestHandlerConfig.java
@@ -36,6 +36,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class ByteResourceHttpRequestHandlerConfig extends ResourceHttpRequestHandler {
 
     public static final String RANGE_START = "BYTE_RANGE_START";
+    public static final String RANGE_END = "BYTE_RANGE_END";
     public static final String MAX_SIZE = "BYTE_MAX_SIZE";
     public static final String RANGE_LEN = "BYTE_RANGE_LEN";
     public static final String SOURCE = "BYTE_SOURCE";
@@ -60,8 +61,14 @@ public class ByteResourceHttpRequestHandlerConfig extends ResourceHttpRequestHan
     protected Resource getResource(HttpServletRequest request) throws IOException {
         String path = (String)request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
         long rangeStart = (long)request.getAttribute(RANGE_START);
-        //如果被分片,每一片大小最多是最大允许上传的10%
-        DataSize ds = DataSize.of((long)(maxFileSize.toMegabytes() * 0.1), DataUnit.MEGABYTES);
+        Long rangeEnd = (Long)request.getAttribute(RANGE_END);
+        DataSize ds = null;
+        if(rangeEnd == null) {//如果请求没有指定结束长度,则自己设置
+            //如果被分片,每一片大小最多是最大允许上传的10%
+            ds = DataSize.of((long) (maxFileSize.toMegabytes() * 0.1), DataUnit.MEGABYTES);
+        }else{//如果设置了结束长度,则以设置为准
+            ds = DataSize.of(rangeEnd,DataUnit.MEGABYTES);
+        }
         try{
             AtomicBoolean ab = new AtomicBoolean(false);
             FileTransmissionDTO dto = new FileTransmissionDTO();
@@ -113,24 +120,38 @@ public class ByteResourceHttpRequestHandlerConfig extends ResourceHttpRequestHan
         if(!StringUtils.hasLength(rangeString)){
             throw new ServletException("Range 不能为空");
         }
+        //苹果公司的设备,会先发一个 bytes=0-1 探测一下视频总共有多大,与其它浏览器,比如谷歌内核的不同,
+        //谷歌内核的请求时会发送 bytes=0- , 返回多少自己设置即可,可以响应206要求继续请求,兼容性比较强
+        //safari的必须先返回总长度,和200响应码
         long rangeStart = Long.valueOf(rangeString.substring(rangeString.indexOf("=") + 1, rangeString.indexOf("-")));
+        String [] sp = rangeString.split("-");
+        Long rangeEnd = null;
+        if(sp.length == 2) {//有发送结束位置
+            rangeStart = Long.parseLong(sp[1]);
+        }
         request.setAttribute(RANGE_START,rangeStart);
+        request.setAttribute(RANGE_END,rangeEnd);
         ByteArrayResource resource = (ByteArrayResource)this.getResource(request);
         if (resource == null) {
             response.sendError(404);
         } else if (HttpMethod.OPTIONS.matches(request.getMethod())) {
             response.setHeader("Allow", this.getAllowHeader());
         } else {
-            response.setStatus(HttpStatus.PARTIAL_CONTENT.value());
-            if(request.getAttribute(CONTENT_TYPE) != null) {
-                response.setContentType((String) request.getAttribute(CONTENT_TYPE));
-            }
-            response.setHeader(HttpHeaders.CONTENT_RANGE, String.format("bytes %s-%s/%s",rangeStart,
-                    (rangeStart + (int)request.getAttribute(RANGE_LEN)-1),
-                    (long)request.getAttribute(MAX_SIZE)));
-            response.setContentLength((int)request.getAttribute(RANGE_LEN));
-            try(ServletOutputStream sos = response.getOutputStream()){
-                sos.write(resource.getByteArray());
+            if(rangeEnd.longValue() == 1l){//探测请求,返回200
+                response.setStatus(HttpStatus.OK.value());
+                response.setContentLength((int) request.getAttribute(MAX_SIZE));
+            }else {
+                response.setStatus(HttpStatus.PARTIAL_CONTENT.value());
+                response.setContentLength((int) request.getAttribute(RANGE_LEN));
+                if(request.getAttribute(CONTENT_TYPE) != null) {
+                    response.setContentType((String) request.getAttribute(CONTENT_TYPE));
+                }
+                response.setHeader(HttpHeaders.CONTENT_RANGE, String.format("bytes %s-%s/%s",rangeStart,
+                        (rangeStart + (int)request.getAttribute(RANGE_LEN)-1),
+                        (long)request.getAttribute(MAX_SIZE)));
+                try(ServletOutputStream sos = response.getOutputStream()){
+                    sos.write(resource.getByteArray());
+                }
             }
         }
     }
