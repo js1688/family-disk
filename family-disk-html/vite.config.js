35c1a0a49a2a3aead71ce19699b7adcac5552e03
js1688 <woshitanjun@icloud.com>
2023-03-24 18:55:56 +0800

    调整目录结构,支持移动端,pc端两个入口打包,nginx可部署两个入口

--- family-disk-html/vite.config.js
+++ family-disk-html/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        pc: path.resolve(__dirname, 'src/pc/index.html'),
+        mobile: path.resolve(__dirname, 'src/mobile/index.html'),
+        index: path.resolve(__dirname, 'index.html')
+      },
+    },
   }
 })
