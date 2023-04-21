69d2e906030423b7391e571907b1071d5bfb19c6
js1688 <woshitanjun@icloud.com>
2023-04-21 15:06:57 +0800

    后端重构目录结构2

--- family-disk-service/family-disk-service-stream/src/main/java/com/jflove/stream/Application.java
+++ family-disk-service/family-disk-service-stream/src/main/java/com/jflove/stream/Application.java
@@ -1,6 +1,7 @@
 package com.jflove.stream;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +13,7 @@ import java.util.concurrent.CountDownLatch;
  * @describe
  */
 @EnableDubbo
+@MapperScan("com.jflove.mapper.*")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws InterruptedException{
