4769451fa755a029522d596f33916c1ac8fb675d
js1688 <woshitanjun@icloud.com>
2023-05-26 21:36:11 +0800

    增加本地磁盘备份路径字段

--- mysql-table-model/DDL/file_disk_config.sql
+++ mysql-table-model/DDL/file_disk_config.sql
@@ -8,7 +8,8 @@ create table file_disk_config
     path        varchar(64) null comment '磁盘地址',
     create_time timestamp   null comment '创建时间',
     update_time timestamp   null comment '修改时间',
-    usable_size int         null comment '磁盘可用(GB)'
+    usable_size int         null comment '磁盘可用(GB)',
+    bakPath     varchar(64) null comment '本地磁盘备份路径'
 )
     comment '可存储文件的磁盘配置' charset = utf8mb3;
 
