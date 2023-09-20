95c5badfafda1a224ecacbc4c8da6912e2b48eec
js1688 <woshitanjun@icloud.com>
2023-09-20 16:41:39 +0800

    去掉备份的,不打算实现备份功能,因为数据备份可以通过运维来解决,没必要去实现 1.常见的服务器都有磁盘阵列方案

--- family-disk-service/family-disk-service-scheduling/src/main/java/com/jflove/scheduling/TaskScheduling.java
+++ family-disk-service/family-disk-service-scheduling/src/main/java/com/jflove/scheduling/TaskScheduling.java
@@ -22,9 +22,6 @@ public class TaskScheduling {
 
     @Autowired
     private CorrectUseSpaceService correctUseSpaceService;
-
-    @Autowired
-    private LocalFileBakService localFileBakService;
     @Autowired
     private ShareClearService shareClearService;
     @Autowired
@@ -53,16 +50,5 @@ public class TaskScheduling {
     public void clearTemp() {
         clearExpireFileService.run();//清除过期无引用文件
         correctUseSpaceService.run();//纠正用户空间使用量
-    }
-
-    /**
-     * 文件备份
-     * 只有本地存储方式的磁盘才有会被执行备份
-     * 如果是使用那种文件存储系统,它们自己会更好的解决备份问题
-     * 一小时执行一次
-     */
-    @Scheduled(fixedDelay = 1000 * 60 * 60)
-    public void fileBak(){
-        localFileBakService.run();//本地存储盘文件备份
     }
 }
